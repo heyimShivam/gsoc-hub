88ce684e9772094a909ac0e3a745dbab9e2c244a
Shivam Chaudhary <shivamthalwal@gmail.com>
2025-05-12 20:33:06 +0530

    pagination is working fine

--- src/pages/AllOrganizations.js
+++ src/pages/AllOrganizations.js
@@ -1,6 +1,7 @@
 import OrganizationInfoCard from "../components/OrganizationInfoCard";
 import { useContext, useEffect, useState } from "react";
 import OrganizationContext from "../context/OrganizationContext";
+import Pagination from '@mui/material/Pagination';
 import "./AllOrganizations.css";
 import FilterNav from "../components/filterNav";
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
@@ -16,26 +17,64 @@ const AllOrganizations = () => {
         });
     }
 
+    const [perPage, setPerPage] = useState(25);
+    const [size, setSize] = useState(perPage);
+    const [current, setCurrent] = useState(1);
+
+    const getData = (current, pageSize) => {
+        // Normally you should get the data from the server
+        return orgsData.slice((current - 1) * pageSize, current * pageSize);
+    };
+
+    let widthofOrganizationComponent;
+    const handleResize = () => {
+        const cardWidth = 250 + 35 + 35 + 35;
+        const componentWidth = widthofOrganizationComponent.clientWidth;
+        let sub = (25 % Math.floor(componentWidth / cardWidth));
+        setPerPage(25 - sub);
+        setSize(perPage);
+    };
+
     useEffect(() => {
         setOrgsData([...orgContext.filteredOrgsData]);
-    }, [
-        orgContext.filteredOrgsData
-    ]);
+        setCurrent(1);
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        widthofOrganizationComponent = document.getElementById("all-organizations-component");
+        handleResize();
+        widthofOrganizationComponent.addEventListener('resize', handleResize);
+
+        return () => {
+            widthofOrganizationComponent.removeEventListener('resize', handleResize);
+        };
+    }, [orgContext.filteredOrgsData]);
+
     return (
         <div className="all-orgs">
             <FilterNav />
             <div className="total-orgs-number">
                 <p className="total-orgs-text">Found {orgsData.length} organizations.</p>
             </div>
-            <div className="all-organizations-component">
+
+            <div className="all-organizations-component" id="all-organizations-component">
                 {
-                    orgsData.map((value, index) => {
+                    getData(current, size).map((value, index) => {
                         return <div className="organization-cards" key={index} >
                             <OrganizationInfoCard {...value} />
                         </div>
                     })
                 }
             </div>
+
+            <div className="table-filter-info">
+                <Pagination
+                    className="pagination-data"
+                    count={Math.ceil(orgsData.length / size)}
+                    page={current}
+                    onChange={(event, value) => { setCurrent(value) }}
+                />
+            </div>
+
             <div className="up-arrow" onClick={scrollToTop}>
                 <ExpandLessIcon />
             </div>
