575c1b11cc812a9d4c44591aa17cf113b88e1aa4
Shivam Chaudhary <shivamthalwal@gmail.com>
2025-05-20 19:53:43 +0530

    update

--- src/components/OrganizationInfoCard.js
+++ src/components/OrganizationInfoCard.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "./OrganizationInfoCard.css";
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PsychologyIcon from '@mui/icons-material/Psychology';
@@ -16,51 +16,53 @@ const OrganizationInfoCard = ({
     githubID,
     activeOrg,
 }) => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    const showMoreOrganizationDetails = () => {
-        navigate(`/organization/${encodeURIComponent(name)}/${encodeURIComponent(githubID)}`, {
-            state: {
-                githubID: githubID,
-                orgName: name,
-            }
-        });
-    }
+    // const showMoreOrganizationDetails = () => {
+    //     navigate(`/organization/${encodeURIComponent(name)}/${encodeURIComponent(githubID)}`, {
+    //         state: {
+    //             githubID: githubID,
+    //             orgName: name,
+    //         }
+    //     });
+    // }
 
-    return (<div className="organization-info-card back-color" onClick={() => { showMoreOrganizationDetails() }}>
-        <div className="org-image-div">
-            <img src={image_url} style={{ backgroundColor: image_background_color }} className="organization-image" alt={name + " image"} />
-            {activeOrg ? <span className="acitve-org-display active-colors">Active</span> :
-                <span className="acitve-org-display inactive-colors">Inactive</span>}
-        </div>
-        <div className="org-deatils-mix">
-            <h3 className="organization-name">{name}</h3>
-            <p className="organization-description">{description}</p>
-            <div className="back-solid-info">
+    // onClick={() => { showMoreOrganizationDetails() }}
+    return (<Link className="a-info-card" to={`/organization/${encodeURIComponent(name)}/${encodeURIComponent(githubID)}`}>
+        <div className="organization-info-card back-color">
+            <div className="org-image-div">
+                <img src={image_url} style={{ backgroundColor: image_background_color }} className="organization-image" alt={name + " image"} />
+                {activeOrg ? <span className="acitve-org-display active-colors">Active</span> :
+                    <span className="acitve-org-display inactive-colors">Inactive</span>}
+            </div>
+            <div className="org-deatils-mix">
+                <h3 className="organization-name">{name}</h3>
+                <p className="organization-description">{description}</p>
+                <div className="back-solid-info">
+                    <div className="title-info-cards">
+                        Technologies
+                    </div>
+                    <div className="organization-cat-year-card">
+                        {
+                            Array.isArray(technologies) ?
+                                technologies.slice(0, 4).map((val, index) => <p className="organization-technologies" key={index}>{val}, </p>) :
+                                <p className="organization-technologies">{technologies}, </p>
+                        }
+                        {
+                            Array.isArray(technologies) && technologies.length > 4 ? <p className="organization-technologies">+ {technologies.length - 4} more.</p> : <></>
+                        }
+                    </div>
+                </div>
                 <div className="title-info-cards">
-                    Technologies
+                    Years
                 </div>
-                <div className="organization-cat-year-card">
-                    {
-                        Array.isArray(technologies) ?
-                            technologies.slice(0, 4).map((val, index) => <p className="organization-technologies" key={index}>{val}, </p>) :
-                            <p className="organization-technologies">{technologies}, </p>
-                    }
-                    {
-                        Array.isArray(technologies) && technologies.length > 4 ? <p className="organization-technologies">+ {technologies.length - 4} more.</p> : <></>
-                    }
+                <div className="organization-cat-year-card years-box">
+                    {year.map((val, index) => {
+                        return <p className="organization-year-card" key={index}>{val}</p>
+                    })}
                 </div>
             </div>
-            <div className="title-info-cards">
-                Years
-            </div>
-            <div className="organization-cat-year-card years-box">
-                {year.map((val, index) => {
-                    return <p className="organization-year-card" key={index}>{val}</p>
-                })}
-            </div>
-        </div>
-    </div>);
+        </div></Link >);
 }
 
 export default OrganizationInfoCard;
